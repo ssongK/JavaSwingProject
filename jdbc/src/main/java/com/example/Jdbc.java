c1e88d1c94758698a046a2ea1c8b7c5f49930e99
ssongK <73736882+ssongK@users.noreply.github.com>
2022-11-10 18:38:24 +0900

    221110 - query method update

--- jdbc/src/main/java/com/example/Jdbc.java
+++ jdbc/src/main/java/com/example/Jdbc.java
@@ -1,15 +1,16 @@
 package com.example;
 
-import java.util.ArrayList;
 import java.sql.*;
 
 public class Jdbc {
     private Connection conn = null;
     private ResultSet rs = null;
     private PreparedStatement insertPstm = null;
-    private Statement selectPstm = null;
+    private PreparedStatement selectPstm = null;
+    private PreparedStatement updatePstm = null;
     private String url = "jdbc:mysql://user-info.cy5xlgs0kg5p.ap-northeast-2.rds.amazonaws.com:3306/user";
     
+    // 생성자 : db와 연결하는 커넥션 객체(conn) 생성
     public Jdbc(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -25,71 +26,104 @@ public class Jdbc {
         }
     }
     
-    public void insert(String name){
+    // 회원가입 : 사용자의 입력 값(name)을 db의 name 컬럼에 저장
+    public void join(String name){
         try {
             String sql = "insert into user(name) values (?)";
             insertPstm = conn.prepareStatement(sql);
             insertPstm.setString(1, name);
             insertPstm.executeUpdate();
+            System.out.println("가입 성공!");
     
         } catch (SQLException e) {
             System.out.println("중복된 이름입니다!");
             System.err.println(e);
             return;
-        } finally {
+        } 
+        finally {
             try {
-                if (rs != null) {
+                if (rs != null) 
                     rs.close();
-                }
-                if (insertPstm != null) {
+
+                if (insertPstm != null) 
                     insertPstm.close();
-                }
-                if (selectPstm != null) {
+                
+                if (selectPstm != null) 
                     selectPstm.close();
-                }
-                if (conn != null) {
+                
+                if (conn != null) 
                     conn.close();
-                }
-            } catch (SQLException e) {
+            } 
+            catch (SQLException e) {
                 e.printStackTrace();
             }
-            System.out.println("가입 성공!");
         }
     }
 
-    public ArrayList<String> searchName(){
-        ArrayList<String> names = new ArrayList<>();
-
+    // 로그인 : 입력한 이름(name)이 db에 있는지 확인
+    public void login(String name){
         try {
             conn = DriverManager.getConnection(url,"myUser","myUser");
-            String sql = "select * from user";
-            selectPstm = conn.createStatement();
-            rs = selectPstm.executeQuery(sql);
-            while (rs.next()) {
-                names.add(rs.getString("name"));
+            String sql = "select name from user where name=(?)";
+            selectPstm = conn.prepareStatement(sql);
+            selectPstm.setString(1, name);
+            rs = selectPstm.executeQuery();
+            rs.next();
+            if(rs.getString("name").equals(name)){
+                System.out.println("로그인 성공!");
             }
-
+            else System.out.println("로그인 실패!");
         } catch (SQLException e) {
-            System.out.println("조회 실패!");
+            System.out.println("로그인 쿼리 실행 실패!");
             System.err.println(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (insertPstm != null) {
-                    insertPstm.close();
-                }
-                if (selectPstm != null) {
-                    selectPstm.close();
-                }
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        } 
+    }
+
+    // 1번 게임 점수 저장
+    public void saveGameScore(String gameName, int gameScore, String userName){
+        try{
+            String sql = "update user set "+gameName+"=(?) where name=(?)";
+            updatePstm = conn.prepareStatement(sql);
+            updatePstm.setInt(1, gameScore);
+            updatePstm.setString(2, userName);
+            updatePstm.executeUpdate();
+            System.out.println("점수 기록 성공!");
+        } 
+        catch (SQLException e) {
+            System.out.println("점수 기록 실패!");
+            System.err.println(e);
         }
-        return names;
+    }
+
+    // 로그아웃 버튼 누르면 모든 객체 close
+    public void logout(){
+        try {
+            if (rs != null) 
+                rs.close();
+            
+            if (insertPstm != null) 
+                insertPstm.close();
+            
+            if (selectPstm != null) 
+                selectPstm.close();
+
+            if (updatePstm != null) 
+                updatePstm.close();
+            
+            if (conn != null) 
+                conn.close();
+            
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Jdbc j = new Jdbc();
+        j.login("ssongk");
+        j.saveGameScore("game3", 29, "ssongk");
+        j.saveGameScore("game2", 129, "ssongk");
+        j.logout();
     }
 }
